d94592ff291af06339d8b60236ecbda2646c2677
Roland Crompton <rollycrompton@gmail.com>
2023-06-27 22:02:30 +0100

    Fully implemented all cage checks

--- ChessHeuristics/src/Heuristics/Deductions/PieceMap.java
+++ ChessHeuristics/src/Heuristics/Deductions/PieceMap.java
@@ -19,6 +19,8 @@ public class PieceMap extends AbstractDeduction{
 //    private PieceNumber = new PieceNumber
 
     private final Map<Coordinate, List<Path>> startLocations = new TreeMap<>();
+    private final Map<Coordinate, Boolean> caged = new TreeMap<>();
+
 
     private final static Map<Integer, String> STANDARD_STARTS = Map.of(
             0, "rook", 1, "knight", 2, "bishop", 3, "queen", 4,
@@ -32,13 +34,12 @@ public class PieceMap extends AbstractDeduction{
                     && this.pawnMap.getSinglePath("white", path.getLast()) != null
                     && Pathfinder.pathsExclusive(this.pawnMap.getSinglePath("white", path.getLast()), Path.of(path, new Coordinate(-1, -1)))
     );
-    Predicate<Path> firstRankCollision = path -> (path.getLast().getY() == 0
+    Predicate<Path> firstRankCollision = path -> !(
+            path.getLast().getY() == 0
             && this.startLocations.containsKey(path.getLast())
-            && Pathfinder.pathsExclusive(
-            this.startLocations.get(path.getLast()).stream()
-                    .filter(path1 -> path1.size() == 1)
-                    .findAny().orElse(new Path()),
-            path));
+            && !this.startLocations.get(path.getLast()).isEmpty()
+            && this.caged.containsKey(path.getLast())
+            && this.caged.get(path.getLast()));
 
     private final Map<String, Predicate<Path>> pathConditions = Map.of(
             //
@@ -66,6 +67,9 @@ public class PieceMap extends AbstractDeduction{
     public Map<Coordinate, List<Path>> getStartLocations() {
         return this.startLocations;
     }
+    public Map<Coordinate, Boolean> getCaged() {
+        return this.caged;
+    }
     @Override
     public List<Observation> getObservations() {
         return this.pawnMap.getObservations();
@@ -76,14 +80,21 @@ public class PieceMap extends AbstractDeduction{
         pawnMap.deduce(board);
 //
         // Bishops
-        findFromOrigin(board, 2, true);
-        findFromOrigin(board, 5, true);
+        findFromOrigin(board, 2, true, false);
+        findFromOrigin(board, 2, true, true);
+        findFromOrigin(board, 5, true, false);
+        findFromOrigin(board, 5, true, true);
         // Royalty
-        findFromOrigin(board, 3, true);
-        findFromOrigin(board, 4, true);
+        findFromOrigin(board, 3, true, false);
+        findFromOrigin(board, 3, true, true);
+        findFromOrigin(board, 4, true, false);
+        findFromOrigin(board, 4, true, true);
         // Rooks
-        findFromOrigin(board, 0, true);
-        findFromOrigin(board, 7, true);
+        findFromOrigin(board, 0, true, false);
+        findFromOrigin(board, 0, true, true);
+        findFromOrigin(board, 7, true, false);
+        findFromOrigin(board, 0, true, true);
+
 
         String colour = "white";
         for (int x = 0; x < 8 ; x++) {
@@ -93,12 +104,12 @@ public class PieceMap extends AbstractDeduction{
 
         return false;
     }
-    private void findFromOrigin(BoardInterface board, int originX, boolean white) {
+    private void findFromOrigin(BoardInterface board, int originX, boolean white, boolean cage) {
 
         Coordinate start = new Coordinate(originX, white ? 0 : 7);
         String pieceName = STANDARD_STARTS.get(originX);
         int escapeLocation = white ? 2 : 5;
-        String pieceCode = (pieceName.equals("knight") ? "n" : pieceName.substring(0, 1));
+        String pieceCode = pieceName.substring(0, 1);
         if (white){
             pieceCode = pieceCode.toUpperCase();
         }
@@ -106,22 +117,39 @@ public class PieceMap extends AbstractDeduction{
         List<Coordinate> candidatePieces = board.getBoardFacts().getCoordinates(white ? "white" : "black", pieceName);
         System.out.println(candidatePieces);
         List<Path> candidatePaths = new LinkedList<>();
-        for (Coordinate target : candidatePieces) {
-            if (pieceName.equals("bishop") && (start.getX() + start.getY()) % 2 != (target.getX() + target.getY()) % 2) {
-                continue;
+        if (cage) {
+            this.caged.put(start, findPath(board, pieceName, pieceCode, start, new Coordinate(4, 4), escapeLocation).isEmpty());
+
+        } else {
+            for (Coordinate target : candidatePieces) {
+                if (pieceName.equals("bishop") && (start.getX() + start.getY()) % 2 != (target.getX() + target.getY()) % 2) {
+                    continue;
+                }
+                Path foundPath = findPath(board, pieceName, pieceCode, start, target, escapeLocation);
+//                    Pathfinder.findShortestPath(
+//                    StandardPieceFactory.getInstance().getPiece(pieceCode),
+//                    start,
+//                    (b, c) -> c.equals(target) || c.getY() == escapeLocation,
+//                    board,
+//                    this.pathConditions.get(pieceName));
+                if (!foundPath.isEmpty()) {
+                    candidatePaths.add(foundPath);
+                }
             }
-            System.out.println(start + " - " + target);
-            Path foundPath = Pathfinder.findShortestPath(
-                    StandardPieceFactory.getInstance().getPiece(pieceCode),
-                    start,
-                    (b, c) -> c.equals(target) || c.getY() == escapeLocation,
-                    board,
-                    this.pathConditions.get(pieceName));
-            if (!foundPath.isEmpty()) {
-                candidatePaths.add(foundPath);
-            }
+            this.startLocations.put(start, candidatePaths);
         }
-        this.startLocations.put(start, candidatePaths);
+
+    }
+
+    private Path findPath(BoardInterface board, String pieceName,
+                          String pieceCode, Coordinate start,
+                          Coordinate target, int escapeLocation){
+        return Pathfinder.findShortestPath(
+                StandardPieceFactory.getInstance().getPiece(pieceCode),
+                start,
+                (b, c) -> c.equals(target) || c.getY() == escapeLocation,
+                board,
+                this.pathConditions.get(pieceName));
     }
 
 }
--- ChessHeuristics/tests/PieceMapTest.java
+++ ChessHeuristics/tests/PieceMapTest.java
@@ -162,6 +162,7 @@ public class PieceMapTest {
         Coordinate start2 = new Coordinate(7, 0);
 
         System.out.println(this.pieceMap.getStartLocations());
+        System.out.println(this.pieceMap.getCaged());
 
         Assertions.assertEquals(1, map.get(start1).size());
         Assertions.assertEquals(1, map.get(start2).size());
@@ -184,8 +185,160 @@ public class PieceMapTest {
 
         Assertions.assertEquals(0, map.get(start1).size());
         Assertions.assertEquals(0, map.get(start2).size());
-//        Path path = Path.of(start1);
-//        Assertions.assertEquals(path, map.get(start1).get(0));
-//        Assertions.assertEquals(Path.of(start2), map.get(start2).get(0));
+
+    }
+
+    @Test
+    void testRooksThroughBishops() {
+        BoardInterface boardInterface = new BoardInterface(BoardBuilder.buildBoard("rnbqkbnr/pppppppp/8/8/2P1PPR1/8/PP1P2PP/1NBQKBN1 w kq - 0 1"));
+        pieceMap.getObservations().forEach(observation -> observation.observe(boardInterface));
+        this.pieceMap.deduce(boardInterface);
+        Map<Coordinate, List<Path>> map = this.pieceMap.getStartLocations();
+        Coordinate start1 = new Coordinate(0, 0);
+        Coordinate start2 = new Coordinate(7, 0);
+
+        System.out.println(this.pieceMap.getStartLocations());
+        System.out.println(this.pawnMapWhite.getPawnOrigins());
+
+        Assertions.assertEquals(0, map.get(start1).size());
+        Assertions.assertEquals(1, map.get(start2).size());
+
+    }
+
+    @Test
+    void testRooksEscape() {
+        BoardInterface boardInterface = new BoardInterface(BoardBuilder.buildBoard("rnbqkbnr/pppppppp/8/2R5/1P4RP/P7/2PPPPP1/1NBQKBN1 w kq - 0 1"));
+        pieceMap.getObservations().forEach(observation -> observation.observe(boardInterface));
+        this.pieceMap.deduce(boardInterface);
+        Map<Coordinate, List<Path>> map = this.pieceMap.getStartLocations();
+        Coordinate start1 = new Coordinate(0, 0);
+        Coordinate start2 = new Coordinate(7, 0);
+
+        System.out.println(this.pieceMap.getStartLocations());
+        System.out.println(this.pawnMapWhite.getPawnOrigins());
+
+        Assertions.assertEquals(2, map.get(start1).size());
+        Assertions.assertEquals(2, map.get(start2).size());
+
+    }
+
+    @Test
+    void testRooksPawnCage() {
+        BoardInterface boardInterface = new BoardInterface(BoardBuilder.buildBoard("rnbqkbnr/pppppppp/8/2R5/6R1/PPP1P2P/3P1PP1/1NBQKBN1 w kq - 0 1"));
+        pieceMap.getObservations().forEach(observation -> observation.observe(boardInterface));
+        this.pieceMap.deduce(boardInterface);
+        Map<Coordinate, List<Path>> map = this.pieceMap.getStartLocations();
+        Coordinate start1 = new Coordinate(0, 0);
+        Coordinate start2 = new Coordinate(7, 0);
+
+        System.out.println(this.pieceMap.getStartLocations());
+        System.out.println(this.pawnMapWhite.getPawnOrigins());
+
+        Assertions.assertEquals(0, map.get(start1).size());
+        Assertions.assertEquals(0, map.get(start2).size());
+
+    }
+
+    @Test
+    void testRooksPawnCageTwo() {
+        BoardInterface boardInterface = new BoardInterface(BoardBuilder.buildBoard("rnbqkbnr/pppppppp/8/8/1RP2R1P/1P1P2P1/P3PP2/1NBQKBN1 w kq - 0 1"));
+        pieceMap.getObservations().forEach(observation -> observation.observe(boardInterface));
+        this.pieceMap.deduce(boardInterface);
+        Map<Coordinate, List<Path>> map = this.pieceMap.getStartLocations();
+        Coordinate start1 = new Coordinate(0, 0);
+        Coordinate start2 = new Coordinate(7, 0);
+
+        System.out.println(this.pieceMap.getStartLocations());
+        System.out.println(this.pawnMapWhite.getPawnOrigins());
+
+        Assertions.assertEquals(2, map.get(start1).size());
+        Assertions.assertEquals(2, map.get(start2).size());
+
+    }
+
+    @Test
+    void testRoyaltyStartLocation() {
+        BoardInterface boardInterface = new BoardInterface(BoardBuilder.buildBoard());
+        pieceMap.getObservations().forEach(observation -> observation.observe(boardInterface));
+        this.pieceMap.deduce(boardInterface);
+        Map<Coordinate, List<Path>> map = this.pieceMap.getStartLocations();
+        Coordinate start1 = new Coordinate(3, 0);
+        Coordinate start2 = new Coordinate(4, 0);
+
+        System.out.println(this.pieceMap.getStartLocations());
+        System.out.println(this.pawnMapWhite.getPawnOrigins());
+
+        Assertions.assertEquals(1, map.get(start1).size());
+        Assertions.assertEquals(1, map.get(start2).size());
+
+    }
+
+    @Test
+    void testRoyaltyStartLocationBlocked() {
+        //Board impossible, may affect future tests
+        BoardInterface boardInterface = new BoardInterface(BoardBuilder.buildBoard("rnbqkbnr/pppppppp/8/8/4QK2/8/PPPPPPPP/RNB2BNR"));
+        pieceMap.getObservations().forEach(observation -> observation.observe(boardInterface));
+        this.pieceMap.deduce(boardInterface);
+        Map<Coordinate, List<Path>> map = this.pieceMap.getStartLocations();
+        Coordinate start1 = new Coordinate(3, 0);
+        Coordinate start2 = new Coordinate(4, 0);
+
+        System.out.println(this.pieceMap.getStartLocations());
+        System.out.println(this.pawnMapWhite.getPawnOrigins());
+
+        Assertions.assertEquals(0, map.get(start1).size());
+        Assertions.assertEquals(0, map.get(start2).size());
+
+    }
+
+    @Test
+    void testRoyaltyEscape() {
+        BoardInterface boardInterface = new BoardInterface(BoardBuilder.buildBoard("rnbqkbnr/pppppppp/8/8/3QK3/2P5/PP1PPPPP/RNB2BNR"));
+        pieceMap.getObservations().forEach(observation -> observation.observe(boardInterface));
+        this.pieceMap.deduce(boardInterface);
+        Map<Coordinate, List<Path>> map = this.pieceMap.getStartLocations();
+        Coordinate start1 = new Coordinate(3, 0);
+        Coordinate start2 = new Coordinate(4, 0);
+
+        System.out.println(this.pieceMap.getStartLocations());
+        System.out.println(this.pawnMapWhite.getPawnOrigins());
+
+        Assertions.assertEquals(1, map.get(start1).size());
+        Assertions.assertEquals(1, map.get(start2).size());
+
+    }
+
+    @Test
+    void testRoyaltyThroughBishop() {
+        BoardInterface boardInterface = new BoardInterface(BoardBuilder.buildBoard("rnbqkbnr/pppppppp/8/8/3QK3/6P1/PPPPPP1P/RNB2BNR"));
+        pieceMap.getObservations().forEach(observation -> observation.observe(boardInterface));
+        this.pieceMap.deduce(boardInterface);
+        Map<Coordinate, List<Path>> map = this.pieceMap.getStartLocations();
+        Coordinate start1 = new Coordinate(3, 0);
+        Coordinate start2 = new Coordinate(4, 0);
+
+        System.out.println(this.pieceMap.getStartLocations());
+        System.out.println(this.pawnMapWhite.getPawnOrigins());
+
+        Assertions.assertEquals(1, map.get(start1).size());
+        Assertions.assertEquals(1, map.get(start2).size());
+
+    }
+
+    @Test
+    void testRoyaltyThroughCagedBishop() {
+        BoardInterface boardInterface = new BoardInterface(BoardBuilder.buildBoard("rnbqkbnr/pppppppp/8/8/3QK3/7P/PPPPPPP1/RNB2BNR"));
+        pieceMap.getObservations().forEach(observation -> observation.observe(boardInterface));
+        this.pieceMap.deduce(boardInterface);
+        Map<Coordinate, List<Path>> map = this.pieceMap.getStartLocations();
+        Coordinate start1 = new Coordinate(3, 0);
+        Coordinate start2 = new Coordinate(4, 0);
+
+        System.out.println(this.pieceMap.getStartLocations());
+        System.out.println(this.pawnMapWhite.getPawnOrigins());
+
+        Assertions.assertEquals(0, map.get(start1).size());
+        Assertions.assertEquals(0, map.get(start2).size());
+
     }
 }
