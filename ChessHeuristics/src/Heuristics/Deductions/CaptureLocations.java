c03723a2db6e32607d38eb7302259d3869c6bd15
Roland Crompton <rollycrompton@gmail.com>
2023-07-14 12:09:30 +0100

    CaptureLocations now works for both colours

--- ChessHeuristics/src/Heuristics/Deductions/CaptureLocations.java
+++ ChessHeuristics/src/Heuristics/Deductions/CaptureLocations.java
@@ -6,6 +6,7 @@ import Heuristics.Observations.PawnNumber;
 import Heuristics.Observations.PieceNumber;
 import Heuristics.Path;
 import Heuristics.Pathfinder;
+import StandardChess.BoardBuilder;
 import StandardChess.Coordinate;
 
 import java.util.*;
@@ -46,11 +47,126 @@ public class CaptureLocations extends AbstractDeduction {
     public boolean deduce(BoardInterface board) {
         int capturesToRemove = 0;
         this.pieceMap.deduce(board);
-        System.out.println("STARTING" + this.pawnMapWhite.getPawnOrigins());
+        int whiteRemovals = reductions(board, true);
+        int blackRemovals = reductions(board, false);
+//        System.out.println("STARTING" + this.pawnMapWhite.getPawnOrigins());
+//
+//        int missingPawns = (8 - this.pawnNumber.getBlackPawns());
+//        int missingNonPawns = (16 - this.pieceNumber.getBlackPieces()) - missingPawns;
+//        Path ofWhichCaged = Path.of(this.pieceMap.getCaged().entrySet().stream()
+//                .filter(Map.Entry::getValue) //Is Caged
+//                .filter(entry -> this.pieceMap.getStartLocations().get(entry.getKey()).isEmpty()) //Is missing
+//                .map(Map.Entry::getKey)
+//                .toList());
+//        int ofWhichBishop = (int) ofWhichCaged.stream()
+//                .filter(coordinate -> coordinate.getX() == 2 || coordinate.getX() == 5) // Is a bishop
+//                .count();
+//
+//        // Rooks are the only piece capable of being both caged and captured on the pawn rank
+//        Path ofWhichRook = Path.of(ofWhichCaged.stream()
+//                .filter(coordinate -> coordinate.getX() == 0 || coordinate.getX() == 7) // Is a rook
+//                .toList());
+//
+//        int ofWhichQueen = ofWhichCaged.size() - ofWhichBishop - ofWhichRook.size();
+//
+//
+//        Map<Integer, Path> reachable = Map.of(0, new Path(), 7, new Path());
+//        if (!ofWhichRook.isEmpty()) {
+//            ofWhichRook.stream().forEach(coordinate2 -> {
+//
+//                board.getBoardFacts().getCoordinates("white", "pawn")
+//                    .stream().filter(coordinate -> coordinate.getY() > 5)
+//                    .forEach(coordinate -> {
+//                            if (!this.pieceMap.findPath(board, "rook", "r", coordinate2, coordinate, 5).isEmpty()) {
+//                                reachable.get(coordinate2.getX()).add(coordinate);
+//                            }
+//                    });
+//            });
+//        }
+//        int maxCaptures = this.pawnMapWhite.capturedPieces() - this.combinedPawnMap.captures("white");
+//
+//        int innaccessibleTakenRooks = 0;
+//
+//        for (int i = 0 ; i < ofWhichRook.size() ; i++){
+//            Coordinate coordinate = ofWhichRook.get(i);
+//            int size = reachable.get(coordinate.getX()).size();
+//            if (size == 0) {
+//                innaccessibleTakenRooks += 1;
+//            } else if (!(ofWhichRook.size() == 1) && !(size > 1)) {
+//                if (reachable.get(coordinate.getX() == 0 ? 7 : 0).size() == 1
+//                        && !reachable.get(coordinate.getX() == 0 ? 7 : 0).contains(coordinate)) {
+//                    innaccessibleTakenRooks += 1;
+//                }
+//            }
+//        }
+//
+//
+//
+//
+//        // Account for bishops being taken on the correct colour
+//        // this is only done in situations where all captures are made by certain pawn paths
+//        List<BiPredicate<Coordinate, Coordinate>> predicates = new LinkedList<>(this.pieceMap.getStartLocations().entrySet().stream()
+//                .filter(entry -> entry.getKey().getX() == 2 || entry.getKey().getX() == 5) //Is a Bishop
+//                .filter(entry -> !(this.pieceMap.getCaged().get(entry.getKey()))) //Is not Caged
+//                .filter(entry -> entry.getValue().isEmpty()) //Is missing
+//                .map(entry -> entry.getKey().getX() == 2 ? LIGHT_TEST : DARK_TEST)
+//                .toList());
+//        System.out.println(predicates.size());
+//
+//        int otherValue = this.combinedPawnMap.getWhitePaths().entrySet().stream()
+//                .filter(entry -> this.combinedPawnMap.getSinglePath("white",  entry.getKey()) != null) //Every path that's a single path
+//                .map(entry -> entry.getValue()
+//                        .stream().map(CombinedPawnMap.PATH_DEVIATION)
+//                        .reduce((i, j) -> i < j ? i : j)
+//                        .orElse(0))
+//                .reduce(0, Integer::sum);
+//        System.out.println(otherValue);
+//
+//        if (otherValue == this.combinedPawnMap.captures("white")) {
+//
+//
+//            for (List<Path> paths : this.combinedPawnMap.getWhitePaths().values()) {
+//                Path path = paths.get(0);
+//                Iterator<BiPredicate<Coordinate, Coordinate>> predicateIterator = predicates.iterator();
+//                while (predicateIterator.hasNext()) {
+//                    BiPredicate<Coordinate, Coordinate> predicate = predicateIterator.next();
+//                    for (int j = 0; j < path.size() - 1; j++) {
+//                        if (predicate.test(path.get(j), path.get(j + 1))) {
+//                            System.out.println(path);
+//                            predicateIterator.remove();
+//                            break;
+//                        }
+//                    }
+//                }
+//            }
+//            if (!predicates.isEmpty()) {
+//                capturesToRemove += predicates.size();
+//            }
+//        }
 
-        int missingPawns = (8 - this.pawnNumber.getBlackPawns());
-        int missingNonPawns = (16 - this.pieceNumber.getBlackPieces()) - missingPawns;
+
+        // Updates the pawn map
+        if (whiteRemovals + blackRemovals != 0) {
+            System.out.println("Rerunning...");
+            this.pawnMapWhite.updateMaxCapturedPieces(whiteRemovals);
+            this.pawnMapBlack.updateMaxCapturedPieces(blackRemovals);
+            this.pieceMap.deduce(board);
+        }
+        System.out.println("WR" + whiteRemovals);
+        System.out.println("BR" + blackRemovals);
+
+
+
+        return true ;
+    }
+
+    private int reductions(BoardInterface board, boolean white) {
+        int capturesToRemove = 0;
+        String colour = white ? "white" : "black";
+        int missingPawns = (8 - (white ? this.pawnNumber.getBlackPawns() : this.pawnNumber.getWhitePawns()));
+        int missingNonPawns = (16 - (white ? this.pawnNumber.getBlackPawns() : this.pawnNumber.getWhitePawns())) - missingPawns;
         Path ofWhichCaged = Path.of(this.pieceMap.getCaged().entrySet().stream()
+                .filter(entry -> entry.getKey().getY() == (white ? 7 : 0))
                 .filter(Map.Entry::getValue) //Is Caged
                 .filter(entry -> this.pieceMap.getStartLocations().get(entry.getKey()).isEmpty()) //Is missing
                 .map(Map.Entry::getKey)
@@ -66,21 +182,27 @@ public class CaptureLocations extends AbstractDeduction {
 
         int ofWhichQueen = ofWhichCaged.size() - ofWhichBishop - ofWhichRook.size();
 
+        System.out.println(colour);
+
+        System.out.println("Ofwhick" + ofWhichBishop);
+
 
         Map<Integer, Path> reachable = Map.of(0, new Path(), 7, new Path());
         if (!ofWhichRook.isEmpty()) {
             ofWhichRook.stream().forEach(coordinate2 -> {
 
-                board.getBoardFacts().getCoordinates("white", "pawn")
-                    .stream().filter(coordinate -> coordinate.getY() > 5)
-                    .forEach(coordinate -> {
-                            if (!this.pieceMap.findPath(board, "rook", "r", coordinate2, coordinate, 5).isEmpty()) {
+                board.getBoardFacts().getCoordinates(colour, "pawn")
+                        .stream().filter(coordinate -> white ? (coordinate.getY() > 5)  : (coordinate.getY() < 2))
+                        .forEach(coordinate -> {
+                            if (!this.pieceMap.findPath(board, "rook", white ? "r" : "R", coordinate2, coordinate, 5).isEmpty()) {
                                 reachable.get(coordinate2.getX()).add(coordinate);
                             }
-                    });
+                        });
             });
         }
-        int maxCaptures = this.pawnMapWhite.capturedPieces() - this.combinedPawnMap.captures("white");
+
+//        int maxCaptures = (white ? this.pawnMapWhite.capturedPieces() : this.pawnMapBlack.capturedPieces())
+//                - this.combinedPawnMap.captures(colour);
 
         int innaccessibleTakenRooks = 0;
 
@@ -98,20 +220,22 @@ public class CaptureLocations extends AbstractDeduction {
         }
 
 
-
-
         // Account for bishops being taken on the correct colour
         // this is only done in situations where all captures are made by certain pawn paths
-        List<BiPredicate<Coordinate, Coordinate>> predicates = new LinkedList<>(this.pieceMap.getStartLocations().entrySet().stream()
-                .filter(entry -> entry.getKey().getX() == 2 || entry.getKey().getX() == 5) //Is a Bishop
-                .filter(entry -> !(this.pieceMap.getCaged().get(entry.getKey()))) //Is not Caged
-                .filter(entry -> entry.getValue().isEmpty()) //Is missing
-                .map(entry -> entry.getKey().getX() == 2 ? LIGHT_TEST : DARK_TEST)
-                .toList());
+        List<BiPredicate<Coordinate, Coordinate>> predicates =
+                new LinkedList<>(this.pieceMap.getStartLocations().entrySet().stream()
+                        .filter(entry -> entry.getKey().getY() == (white ? 7 : 0)) //Correct colour
+                        .filter(entry -> entry.getKey().getX() == 2 || entry.getKey().getX() == 5) //Is a Bishop
+                        .filter(entry -> !(this.pieceMap.getCaged().get(entry.getKey()))) //Is not Caged
+                        .filter(entry -> entry.getValue().isEmpty()) //Is missing
+                        .map(entry -> ((entry.getKey().getX() + entry.getKey().getY()) % 2 == 0)  ? DARK_TEST : LIGHT_TEST)
+                        .toList());
         System.out.println(predicates.size());
 
-        int otherValue = this.combinedPawnMap.getWhitePaths().entrySet().stream()
-                .filter(entry -> this.combinedPawnMap.getSinglePath("white",  entry.getKey()) != null) //Every path that's a single path
+        Map<Coordinate, List<Path>> paths = white ? this.combinedPawnMap.getWhitePaths() : this.combinedPawnMap.getBlackPaths();
+
+        int otherValue = paths.entrySet().stream()
+                .filter(entry -> this.combinedPawnMap.getSinglePath(colour,  entry.getKey()) != null) //Every path that's a single path
                 .map(entry -> entry.getValue()
                         .stream().map(CombinedPawnMap.PATH_DEVIATION)
                         .reduce((i, j) -> i < j ? i : j)
@@ -119,11 +243,11 @@ public class CaptureLocations extends AbstractDeduction {
                 .reduce(0, Integer::sum);
         System.out.println(otherValue);
 
-        if (otherValue == this.combinedPawnMap.captures("white")) {
+        if (otherValue == this.combinedPawnMap.captures(colour)) {
 
 
-            for (List<Path> paths : this.combinedPawnMap.getWhitePaths().values()) {
-                Path path = paths.get(0);
+            for (List<Path> pathList : paths.values()) {
+                Path path = pathList.get(0);
                 Iterator<BiPredicate<Coordinate, Coordinate>> predicateIterator = predicates.iterator();
                 while (predicateIterator.hasNext()) {
                     BiPredicate<Coordinate, Coordinate> predicate = predicateIterator.next();
@@ -141,15 +265,7 @@ public class CaptureLocations extends AbstractDeduction {
             }
         }
 
-
-        // Updates the pawn map
-        if (ofWhichQueen + ofWhichBishop + innaccessibleTakenRooks + capturesToRemove != 0) {
-            System.out.println("Rerunning...");
-            this.pawnMapWhite.updateMaxCapturedPieces(ofWhichQueen + ofWhichBishop + innaccessibleTakenRooks + capturesToRemove);
-            this.pieceMap.deduce(board);
-        }
-
-
-        return true ;
+        return ofWhichQueen + ofWhichBishop + innaccessibleTakenRooks + capturesToRemove;
     }
+
 }
--- ChessHeuristics/tests/CaptureLocationTest.java
+++ ChessHeuristics/tests/CaptureLocationTest.java
@@ -37,6 +37,19 @@ public class CaptureLocationTest {
     }
 
     @Test
+    void testUntakeableBlack() {
+        BoardInterface boardInterface = new BoardInterface(BoardBuilder.buildBoard("rnbqkbnr/ppppp2p/8/4p3/8/8/PPPPPPPP/RNBQK2R w KQkq - 0 1"));
+
+        pieceMap.getObservations().forEach(observation -> observation.observe(boardInterface));
+        this.pieceMap.deduce(boardInterface);
+
+        this.captureLocations.deduce(boardInterface);
+        System.out.println(pawnMapBlack.getPawnOrigins());
+        Assertions.assertEquals(1, pawnMapBlack.getPawnOrigins().get(new Coordinate(4, 4)).size());
+
+    }
+
+    @Test
     void testUntakeableWithRook() {
         BoardInterface boardInterface = new BoardInterface(BoardBuilder.buildBoard("rnbqk3/ppppppp1/7p/8/2P5/8/2PPPPPP/RNBQKBNR w KQq - 0 1"));
 
@@ -46,6 +59,19 @@ public class CaptureLocationTest {
         this.captureLocations.deduce(boardInterface);
         System.out.println(pawnMapWhite.getPawnOrigins());
         Assertions.assertEquals(1, pawnMapWhite.getPawnOrigins().get(new Coordinate(2, 3)).size());
+
+    }
+
+    @Test
+    void testUntakeableWithRookBlack() {
+        BoardInterface boardInterface = new BoardInterface(BoardBuilder.buildBoard("rnbqkbnr/ppppp2p/8/4p3/8/7P/PPPPPPP1/RNBQK3 w Qkq - 0 1"));
+
+        pieceMap.getObservations().forEach(observation -> observation.observe(boardInterface));
+        this.pieceMap.deduce(boardInterface);
+
+        this.captureLocations.deduce(boardInterface);
+        System.out.println(pawnMapBlack.getPawnOrigins());
+        Assertions.assertEquals(1, pawnMapBlack.getPawnOrigins().get(new Coordinate(4, 4)).size());
 
     }
 
@@ -63,6 +89,19 @@ public class CaptureLocationTest {
     }
 
     @Test
+    void testUntakeableCapturedRookBlack() {
+        BoardInterface boardInterface = new BoardInterface(BoardBuilder.buildBoard("rnbqkbnr/pp1ppppp/8/8/8/NP6/PpPPPPPP/2BQKBNR w Kk - 0 1"));
+
+        pieceMap.getObservations().forEach(observation -> observation.observe(boardInterface));
+        this.pieceMap.deduce(boardInterface);
+
+        this.captureLocations.deduce(boardInterface);
+        System.out.println(pawnMapBlack.getPawnOrigins());
+        Assertions.assertEquals(1, pawnMapBlack.getPawnOrigins().get(new Coordinate(1, 1)).size());
+
+    }
+
+    @Test
     void testUntakeableRook() {
         BoardInterface boardInterface = new BoardInterface(BoardBuilder.buildBoard("1nbqkb1r/1pppppPp/p6n/8/8/8/PPPP1P1P/RNBQKBNR w KQk - 0 1"));
 
@@ -72,6 +111,19 @@ public class CaptureLocationTest {
         this.captureLocations.deduce(boardInterface);
         System.out.println(pawnMapWhite.getPawnOrigins());
         Assertions.assertEquals(1, pawnMapWhite.getPawnOrigins().get(new Coordinate(6, 6)).size());
+
+    }
+
+    @Test
+    void testUntakeableRookBlack() {
+        BoardInterface boardInterface = new BoardInterface(BoardBuilder.buildBoard("rnbqkbnr/pppp1p1p/8/8/8/P6N/1PPPPPpP/1NBQKB1R w Kk - 0 1"));
+
+        pieceMap.getObservations().forEach(observation -> observation.observe(boardInterface));
+        this.pieceMap.deduce(boardInterface);
+
+        this.captureLocations.deduce(boardInterface);
+        System.out.println(pawnMapBlack.getPawnOrigins());
+        Assertions.assertEquals(1, pawnMapBlack.getPawnOrigins().get(new Coordinate(6, 1)).size());
 
     }
 
@@ -90,6 +142,20 @@ public class CaptureLocationTest {
     }
 
     @Test
+    void testBishopColoursOneBlack() {
+        BoardInterface boardInterface = new BoardInterface(BoardBuilder.buildBoard("rnbqkbnr/p1pppppp/8/3p4/8/3P4/PPP1PPPP/RN1QKB1R w KQkq - 0 1"));
+
+        pieceMap.getObservations().forEach(observation -> observation.observe(boardInterface));
+        this.pieceMap.deduce(boardInterface);
+
+        this.captureLocations.deduce(boardInterface);
+        System.out.println(pawnMapBlack.getPawnOrigins());
+        System.out.println(combinedPawnMap.getBlackPaths());
+        Assertions.assertEquals(0, pawnMapBlack.getPawnOrigins().get(new Coordinate(3, 4)).size());
+
+    }
+
+    @Test
     void testBishopColoursTwo() {
         BoardInterface boardInterface = new BoardInterface(BoardBuilder.buildBoard("rnbqk2r/pppppp1p/6p1/8/4P3/8/PPPPPP1P/RNBQKBNR w KQkq - 0 1"));
 
@@ -102,4 +168,19 @@ public class CaptureLocationTest {
         Assertions.assertEquals(0, pawnMapWhite.getPawnOrigins().get(new Coordinate(4, 3)).size());
 
     }
+
+    @Test
+    void testBishopColoursTwoBlack() {
+        BoardInterface boardInterface = new BoardInterface(BoardBuilder.buildBoard("rnbqkbnr/pppppp1p/8/4p3/8/6P1/PPPPPP1P/RNBQK2R w KQkq - 0 1"));
+
+        pieceMap.getObservations().forEach(observation -> observation.observe(boardInterface));
+        this.pieceMap.deduce(boardInterface);
+
+        this.captureLocations.deduce(boardInterface);
+        System.out.println(pawnMapBlack.getPawnOrigins());
+        System.out.println(combinedPawnMap.getBlackPaths());
+        Assertions.assertEquals(0, pawnMapBlack.getPawnOrigins().get(new Coordinate(4, 4)).size());
+
+    }
+
 }
