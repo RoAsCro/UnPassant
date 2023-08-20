0f1cbd52d0db71e687fb27ab185afb0f47611656
Roland Crompton <rollycrompton@gmail.com>
2023-08-20 21:53:22 +0100

    Documentation for ChessBoard and StandardBoard

--- ReverseChess/src/StandardChess/ChessBoard.java
+++ ReverseChess/src/StandardChess/ChessBoard.java
@@ -1,28 +1,89 @@
 package StandardChess;
 
+/**
+ * An interface for ChessBoards providing methods for setting and getting data
+ * about the chess board it represents.
+ * @author Roland Crompton
+ */
 public interface ChessBoard {
-    final static int LENGTH = 8;
+    /**The number of squares long a chess board is*/
+    int LENGTH = 8;
 
-    public Piece at(Coordinate coordinate);
+    /**
+     * Returns the Piece at the given Coordinate. If there is no piece at that location,
+     * returns an instance of NullPiece.
+     * @param coordinate the Coordinate of the Piece
+     * @return the Piece at the Coordinate
+     */
+    Piece at(Coordinate coordinate);
 
-    public String getTurn();
+    /**
+     * Returns the player whose turn it is as a String, "white" or "black"
+     * @return the player whose turn it is
+     */
+    String getTurn();
 
-    public BoardReader getReader();
+    /**
+     * Sets the player whose turn it is as a String, "white" or "black"
+     * @param turn the player whose turn it is
+     */
+    void setTurn(String turn);
 
-    public void setTurn(String turn);
+    /**
+     * Returns the BoardReader for the ChessBoard.
+     * @return this ChessBoard's BoardReader
+     */
+    BoardReader getReader();
 
-    public void place(Coordinate coordinate, Piece piece);
+    /**
+     * Places the given Piece at the given Coordinate on the Board.
+     * @param coordinate the Coordinate the Piece will be placed at
+     * @param piece the Piece to place at the Coordinate
+     */
+    void place(Coordinate coordinate, Piece piece);
 
-    public void remove(Coordinate coordinate);
+    /**
+     * Removes the Piece currently at the given location.
+     * @param coordinate the Coordinate of the Piece to remove
+     */
+    void remove(Coordinate coordinate);
 
-    public boolean canCastle(String pieceSide, String colour);
+    /**
+     * Returns whether the king of the given colour can castle on the given side.
+     * The colour should be "white" or "black", and the piece side should be "queen"
+     * or "king".
+     * <p></p>
+     * In reverse play, this represents whether the king MUST castle on the given side.
+     * @param pieceSide the castle side, "queen" or "king"
+     * @param colour the colour of the king "white" or "black"
+     * @return whether the king of the given colour can castle on the given side
+     */
+    boolean canCastle(String pieceSide, String colour);
 
-    public void setCastle(String pieceSide, String colour, boolean canCastle);
+    /**
+     * Sets whether the king of the given colour can castle on the given side.
+     * The colour should be "white" or "black", and the piece side should be "queen"
+     * or "king".
+     *  <p></p>
+     *  In reverse play, this represents whether the king MUST castle on the given side.
+     * @param pieceSide the castle side, "queen" or "king"
+     * @param colour the colour of the king "white" or "black"
+     * @param canCastle whether the side should be allowed to castle
+     */
+    void setCastle(String pieceSide, String colour, boolean canCastle);
 
-//    public void setUnCastle(String pieceSide, String colour);
-
+    /**
+     * In forward play, returns the location to which a pawn may move to capture en passant.
+     * In reverse play, returns the location of the pawn which must move double move in reverse.
+     * If this isn't set, returns a Null Coordinate.
+     * @return the en passant Coordinate
+     */
     Coordinate getEnPassant();
 
+    /**
+     * Sets the en passant Coordinate.
+     * @param coordinate the Coordinate to set the en passant Coordinate to
+     */
     void setEnPassant(Coordinate coordinate);
 
 }
--- ReverseChess/src/StandardChess/StandardBoardReader.java
+++ ReverseChess/src/StandardChess/StandardBoardReader.java
@@ -6,28 +6,36 @@ import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 /**
- * An
- *
+ * A BoardReader that works with a StandardBoard.
  * @author Roland Crompton
  */
 public class StandardBoardReader implements BoardReader {
-
+    /**The ASCII value of lowercase a*/
     private static final int ASCII_LOWERCASE_A = 97;
-
+    /**A List of the directions a knight can move in*/
+    private static final List<Coordinate> KNIGHT_DIRECTIONS = List.of(Coordinates.KNIGHT_DIRECTIONS);
+    /**The standard directions a Piece can move in*/
     private static final List<Coordinate> STANDARD_DIRECTIONS = List.of(
             Coordinates.UP, Coordinates.DOWN, Coordinates.LEFT,
             Coordinates.RIGHT, Coordinates.UP_RIGHT, Coordinates.UP_LEFT,
             Coordinates.DOWN_LEFT, Coordinates.DOWN_RIGHT);
-    private static final List<Coordinate> KNIGHT_DIRECTIONS = List.of(Coordinates.KNIGHT_DIRECTIONS);
-
+    /**The StandardBoard that this BoardReader iterates over*/
     private final StandardBoard board;
-
+    /**The current Coordinate location of the BoardReader*/
     private Coordinate current = new Coordinate(0, 0);
 
+    /**
+     * Constructs a BoardReader over the given StandardBoard.
+     * @param board the StandardBoard this BoardReader is to iterate over
+     */
     public StandardBoardReader(StandardBoard board) {
         this.board = board;
     }
 
+    /**
+     * Outputs the ChessBoard as a FEN.
+     * @return a FEN representation of the ChessBoard
+     */
     @Override
     public String toFEN() {
         StringBuilder builder = new StringBuilder();
@@ -96,6 +104,11 @@ public class StandardBoardReader implements BoardReader {
         return builder.toString();
     }
 
+    /**
+     * Returns whether the specified Coordinate is in check.
+     * @param kingLocation the Coordinate location of the king being checked
+     * @return true if the king at the given location is in check
+     */
     @Override
     public boolean inCheck(Coordinate kingLocation) {
         Predicate<Coordinate> canAttackKing =
@@ -119,35 +132,65 @@ public class StandardBoardReader implements BoardReader {
         return false;
     }
 
+    /**
+     * Returns the current location of the BoardReader.
+     * @return a Coordinate of the current location of the Board Reader
+     */
     @Override
     public Coordinate getCoord() {
         return this.current;
     }
 
+    /**
+     * Gets the next Coordinate in the given direction.
+     * @param direction the direction to get
+     * @return the Coordinate in the given direction
+     */
     private Coordinate getNext(Coordinate direction) {
         return Coordinates.add(this.current, direction);
     }
 
+    /**
+     * Moves Board reader to the next Coordinate in the given direction.
+     * @param direction the direction to move the BoardReader in
+     * @return the Piece at the new location of the BoardReader
+     */
     @Override
     public Piece next(Coordinate direction) {
         this.current = getNext(direction);
         return this.board.at(this.current);
     }
 
+    /**
+     * Returns true if the next Coordinate in the given direction is on the ChessBoard.
+     * @param direction the direction to check
+     * @return true if the next Coordinate in the given direction is on the ChessBoard
+     */
     @Override
     public boolean hasNext(Coordinate direction) {
         Coordinate nextCoord = getNext(direction);
-        int nextX = nextCoord.getX();
-        int nextY = nextCoord.getY();
-        return nextX < ChessBoard.LENGTH && nextX >= 0
-                && nextY < ChessBoard.LENGTH && nextY >= 0;
+        return Coordinates.inBounds(nextCoord);
     }
 
+    /**
+     * Moves Board reader to the next Coordinate in the given direction while the given condition remains
+     * true.
+     * @param direction the direction to move the BoardReader in
+     * @param condition the condition that needs to remain true for the BoardReader to continue moving
+     */
     @Override
     public void nextWhile(Coordinate direction, Predicate<Coordinate> condition) {
         nextWhile(direction, condition, p -> {});
     }
 
+    /**
+     * Moves Board reader to the next Coordinate in the given direction while the given condition remains
+     * true for the next Coordinate, carrying out the given function at each Piece it crosses, starting with the
+     * Coordinate the BoardReader is currently at.
+     * @param direction the direction to move the BoardReader in
+     * @param condition the condition that needs to remain true for the BoardReader to continue moving
+     * @param function the function carried out on each Piece
+     */
     @Override
     public void nextWhile(Coordinate direction, Predicate<Coordinate> condition, Consumer<Piece> function) {
         function.accept(this.board.at(this.current));
@@ -157,11 +200,21 @@ public class StandardBoardReader implements BoardReader {
         }
     }
 
+    /**
+     * Moves Board reader across the whole board while the given condition remains true for the next Coordinate.
+     * @param condition the condition that needs to remain true for the BoardReader to continue moving
+     */
     @Override
     public void wholeBoard(Predicate<Coordinate> condition) {
         wholeBoard(condition, f -> {});
     }
 
+    /**
+     * Moves Board reader across the whole board  while the given condition remains true for the next Coordinate,
+     * carrying out the given function at each Piece it crosses starting with the Coordinate it's currently at.
+     * @param condition the condition that needs to remain true for the BoardReader to continue moving
+     * @param function the function carried out on each Piece
+     */
     @Override
     public void wholeBoard(Predicate<Coordinate> condition, Consumer<Piece> function) {
         for (int y = 0 ; y < ChessBoard.LENGTH ; y++) {
@@ -169,12 +222,14 @@ public class StandardBoardReader implements BoardReader {
         }
     }
 
+    /**
+     * Moves the BoardReader to the given Coordinate.
+     * @param target the Coordinate to move the BoardReader
+     * @return the Piece at the given location
+     */
     @Override
     public Piece to(Coordinate target) {
-
         this.current = target;
         return this.board.at(this.current);
     }
-
-
 }
